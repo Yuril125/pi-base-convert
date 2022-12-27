f42809fd247563650c060842c7f9e3157362839d
Yuril <YurilAvali@protonmail.com>
2022-12-27 16:35:58 -0500

    Add debug and progress prints

--- code/main.cpp
+++ code/main.cpp
@@ -8,7 +8,9 @@
 // Change the formula for num_out_radix_digits if IN_RADIX is not 10
 constexpr unsigned long int IN_RADIX = 10;
 constexpr long int OUT_RADIX = 27;
-constexpr bool DEBUG_PRINT = false;
+constexpr bool PRINT_DEBUG = true;
+constexpr bool PRINT_NUMS = false;
+constexpr bool PRINT_PROGRESS = true;
 
 void make_upper(char *str) {
   for (size_t i{}; str[i]; i++) {
@@ -54,52 +56,61 @@ char *tostr_mpz_b27(const mpz_t &bigint) {
 }
 
 int main() {
-  FILE *input = fopen(R"(..\large_files\pi_dec_1t_01\pi_dec_16MiB.txt)", "r");
+  FILE *input = fopen(R"(..\large_files\pi_dec_1t_01\pi_dec_1MiB.txt)", "rb");
+  if constexpr (PRINT_DEBUG) printf("File opened.\n");
 
   // Get file size:
   struct stat st;
   fstat(fileno(input), &st);
+  if constexpr (PRINT_DEBUG) printf("File size: %zu bytes\n", st.st_size);
   size_t buffer_size = st.st_size + 1;
 
   // Load file into buffer:
+  if constexpr (PRINT_DEBUG) printf("Loading file... ");
   auto *buffer = new char[buffer_size];
-  if (!fread(buffer, buffer_size + 1, 1, input)) {
-    throw std::runtime_error{"Error reading file"};
-  }
+  fread(buffer, buffer_size + 1, 1, input);
   fclose(input);
+  if constexpr (PRINT_DEBUG) printf("Done.\n");
 
   // Load buffer into mpz_t:
   mpz_t pi;
   mpz_init_set_str(pi, buffer, IN_RADIX);
   delete[] buffer;
 
-  if constexpr (DEBUG_PRINT) {
-    print_mpz(pi, IN_RADIX);
-  }
+  if constexpr (PRINT_NUMS) print_mpz(pi, IN_RADIX);
 
   // Number of decimal digits after the decimal point
   size_t num_in_radix_digits = mpz_sizeinbase(pi, IN_RADIX) - 1;
-  printf("num_in_radix_digits: %zu\n", num_in_radix_digits);
+  if constexpr (PRINT_DEBUG)
+    printf("num_in_radix_digits: %zu\n", num_in_radix_digits);
+  if (!num_in_radix_digits) throw std::runtime_error{"Error reading file."};
 
   // Calculate the number of output digits we can reasonably produce,
   // given the number of input digits we have:
   size_t num_out_radix_digits = num_in_radix_digits / log10(OUT_RADIX);
-  printf("num_out_radix_digits: %zu\n", num_out_radix_digits);
+  if constexpr (PRINT_DEBUG)
+    printf("num_out_radix_digits: %zu\n", num_out_radix_digits);
 
   // Do the actual conversion:
+  if constexpr (PRINT_PROGRESS) printf("Converting to base %ld... ", OUT_RADIX);
   for (size_t i{}; i < num_out_radix_digits; i++) {
     mpz_mul_si(pi, pi, OUT_RADIX);
   }
+  if constexpr (PRINT_PROGRESS)
+    printf("Done.\nDiving by input base %lu... ", IN_RADIX);
   for (size_t i{}; i < num_in_radix_digits; i++) {
     mpz_tdiv_q_ui(pi, pi, IN_RADIX);
   }
+  if constexpr (PRINT_PROGRESS) printf("Done.\n");
 
   // Write result to output:
+  if constexpr (PRINT_PROGRESS) printf("Writing to output file... ");
   auto out = tostr_mpz_b27(pi);
   FILE *fout = fopen("out.txt", "w");
   fprintf(fout, "%s", out);
   fclose(fout);
   delete[] out;
+  if constexpr (PRINT_PROGRESS) printf("Done.\n");
 
   return 0;
 }
